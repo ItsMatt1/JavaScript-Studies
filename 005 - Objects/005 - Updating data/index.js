73a55817f5f4dccec77f0fea1ef49fd2189d7f36
ItsMatt1 <matedits03216@gmail.com>
2022-08-15 14:27:41 -0300

    new studies

---
+++ 005 - Objects/005 - Updating data/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Teste = {
+    tamanho: 45,
+    estoque: true
+}
+
+Teste.tamanho = 42
+
+console.log(Teste)
+Teste.tamanho = 40
+
+console.log(Teste)
+Teste.tamanho = 55
+
+console.log(Teste)
---
+++ 005 - Objects/006 - Deleting Data/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Teste = {
+    tamanho: 45,
+    estoque: true
+}
+
+delete Teste.tamanho
+
+console.log(Teste)
