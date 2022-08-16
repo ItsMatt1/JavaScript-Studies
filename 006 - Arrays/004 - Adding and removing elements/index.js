7bde307cf552600aa333c9e2da04d5b4d8396f82
ItsMatt1 <matedits03216@gmail.com>
2022-08-16 17:07:34 -0300

    studying arrays

---
+++ 006 - Arrays/004 - Adding and removing elements/index.js
@@ -0,0 +1,22 @@
+//PUSH coloca o item no final
+//POP remove o ultimo item 
+//SHIFT remove item do inicio
+//UNSHIFT coloca o item no inicio
+//SLICE
+//SPLICE
+
+let arr = [1, 2, 3, 4, 5]
+
+arr.push(6)
+
+arr.unshift(0)
+
+console.log(arr)
+
+arr.shift()
+
+console.log(arr)
+
+arr.pop()
+
+console.log(arr)
