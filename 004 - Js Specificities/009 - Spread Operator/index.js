c5efdec91b82c05e9d77d0a33e574e20ea0938d4
ItsMatt1 <matedits03216@gmail.com>
2022-08-16 17:07:34 -0300

    studying arrays

---
+++ 004 - Js Specificities/009 - Spread Operator/index.js
@@ -0,0 +1,5 @@
+const num = [1, 2, 8, 4, 5]
+
+console.log(...num)
+
+console.log(Math.max(...num))
---
+++ 006 - Arrays/003 - Destructing/index.js
@@ -0,0 +1,9 @@
+const [a, b, c] = ["Arroz", "Feijão", "Bife"]
+
+const comida = ["Arroz", "Feijão", "Bife"]
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log(comida[0])
